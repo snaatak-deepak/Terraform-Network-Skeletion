92ed4c90913133d188b9b68f6ed41c47a6b52667
iabhishek-snaatak <abhishek.saini.snaatak@mygurukulam.co>
2025-09-15 19:22:43 +0530

    Add files via upload

---
+++ terraform-vpc/variable.tf
@@ -0,0 +1,27 @@
+
+#>>>>>>>>>>>>>>>>>>>VPC-VARIABLES>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_enable_dns_support" {
+  type    = bool
+  default = true
+}
+
+variable "vpc_enable_dns_hostnames"{
+  type    = bool
+  default = true
+}
+
+variable "vpc_tags" {
+  type    = map(string)
+  default = {
+    Name = "dev-vpc"
+    Enviroment = "dev"
+    Owner = "abhishek"
+  }
+}
+
